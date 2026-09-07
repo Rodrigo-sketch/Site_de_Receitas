1b9eeb6be2335ec6a8d14889434276c2ff48bf68
AssemblyAdmin <ivan.rupt@gmail.com>
2026-09-07 20:52:42 +0100

    update repository

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -1,14 +1,39 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.Data.SqlClient;
 using Models;
 
 namespace Repository
 {
     public class UserRepository
     {
-        string connString =
+        string connString = "Server=(localdb)\\MSSQLLocalDB;Database=recipes;Trusted_Connection=True;TrustServerCertificate=True;";
 
         public List<User> GetUsers()
         {
+            List<User> users = new List<User>();
+
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand("SELECT id, name, password, email FROM Users", conn);
+
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                users.Add(new User
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Password = reader.GetString(2),
+                    Email = reader.GetString(3)
+                }
+                );
+            }
+
+            conn.Close();
+            
+            return users;
 
         }
         public UserRepository() { }
--- Site_de_Receitas/Pages/Index.cshtml.cs
+++ Site_de_Receitas/Pages/Index.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Repository;
+
+
 
 namespace Site_de_Receitas.Pages
 {
     public class IndexModel : PageModel
     {
+
+        public User user;
         public void OnGet()
         {
+            UserRepository  userRepository = new UserRepository();
+
+            user = userRepository.GetUsers().FirstOrDefault();
+
         }
     }
 }
